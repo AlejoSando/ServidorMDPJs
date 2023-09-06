b8dd46bf8ff6ca646ad6a3a9c9f3f08a95be9d54
Alejo Sandoval <alejosandoval2004@gmail.com>
2023-09-06 18:02:58 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors'
+import dotenv from 'dotenv'
 import { getActividad, getAll, getDatos, getDireccion, getHorarios, getLink, getSubCategoria, getTelefono } from './database/db.js';
 
+dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 8080;
 
